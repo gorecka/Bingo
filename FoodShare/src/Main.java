57e1e03629f94fae52b3e66d85b23633f9e072b4
mzych1 <magda0zych@gmail.com>
2022-01-06 14:03:54 +0100

    Funkcja tworząca kontener i agenty

---
+++ FoodShare/src/Main.java
@@ -0,0 +1,25 @@
+import jade.wrapper.AgentContainer;
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.StaleProxyException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Profile profile = new ProfileImpl();
+        AgentContainer container = Runtime.instance().createMainContainer(profile);
+
+        try {
+            AgentController giver1 = container.createNewAgent("W1", "giver.GiverAgent", null);
+            AgentController advertisingColumn1 = container.createNewAgent("Slup",
+                    "advertisingColumn.AdvertisingColumnAgent", null);
+
+            advertisingColumn1.start();
+            giver1.start();
+        } catch (StaleProxyException e) {
+            e.printStackTrace();
+        }
+    }
+}
