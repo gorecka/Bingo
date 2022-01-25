f08f7bb72471706c3eff8f1ee9ee0d8d3a9bb734
Grzegorz Kuduk <grzegorz07@gmail.com>
2022-01-25 20:23:43 +0100

    add getters and setters

--- FoodShare/src/advertisingColumn/data/Offer.java
+++ FoodShare/src/advertisingColumn/data/Offer.java
@@ -3,11 +3,24 @@ package advertisingColumn.data;
 import java.util.Date;
 import java.util.List;
 
+enum OfferStatus {
+    NEW,
+    WAITING_FOR_TAKERS,
+    WAITING_FOR_GIVER,
+    CLOSED
+} // idk, pewnie się zmieni
+
+enum ItemStatus {
+    FRESH,
+    CLOSE_EXPIRATION,
+    IMPERISHABLE
+} // pewnie też do zmiany
+
 public class Offer {
     private int offerId;
     private String name;
     private String description;
-    private String state; // enum? jakie mogą być stany?
+    private OfferStatus offerStatus;
     private Date bestBeforeDate;
     private Date creationDate;
 
@@ -15,11 +28,151 @@ public class Offer {
     private List<User> possibleReceivers;
     private User chosenReceiver;
 
-    private String status; // zrobić enum?
+    private ItemStatus itemStatus;
     private String receiptPlace;
     private Date receiptDate;
     private Date receiptDetailsSettled;
     private Date receiptConfirmationDate;
     private int rating;
     private Date reviewDate;
+
+    public Offer() {}
+    public Offer(int offerId, String name, String description, OfferStatus offerStatus, Date bestBeforeDate, User author, ItemStatus itemStatus) {
+        this.offerId = offerId;
+        this.name = name;
+        this.description = description;
+        this.offerStatus = offerStatus;
+        this.bestBeforeDate = bestBeforeDate;
+        creationDate = new Date();
+        this.author = author;
+        this.itemStatus = itemStatus;
+    }
+
+    public int getOfferId() {
+        return offerId;
+    }
+
+    public void setOfferId(int offerId) {
+        this.offerId = offerId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getBestBeforeDate() {
+        return bestBeforeDate;
+    }
+
+    public void setBestBeforeDate(Date bestBeforeDate) {
+        this.bestBeforeDate = bestBeforeDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public List<User> getPossibleReceivers() {
+        return possibleReceivers;
+    }
+
+    public void setPossibleReceivers(List<User> possibleReceivers) {
+        this.possibleReceivers = possibleReceivers;
+    }
+
+    public User getChosenReceiver() {
+        return chosenReceiver;
+    }
+
+    public void setChosenReceiver(User chosenReceiver) {
+        this.chosenReceiver = chosenReceiver;
+    }
+
+    public OfferStatus getOfferStatus() {
+        return offerStatus;
+    }
+
+    public void setOfferStatus(OfferStatus offerStatus) {
+        this.offerStatus = offerStatus;
+    }
+
+    public ItemStatus getItemStatus() {
+        return itemStatus;
+    }
+
+    public void setItemStatus(ItemStatus itemStatus) {
+        this.itemStatus = itemStatus;
+    }
+
+    public String getReceiptPlace() {
+        return receiptPlace;
+    }
+
+    public void setReceiptPlace(String receiptPlace) {
+        this.receiptPlace = receiptPlace;
+    }
+
+    public Date getReceiptDate() {
+        return receiptDate;
+    }
+
+    public void setReceiptDate(Date receiptDate) {
+        this.receiptDate = receiptDate;
+    }
+
+    public Date getReceiptDetailsSettled() {
+        return receiptDetailsSettled;
+    }
+
+    public void setReceiptDetailsSettled(Date receiptDetailsSettled) {
+        this.receiptDetailsSettled = receiptDetailsSettled;
+    }
+
+    public Date getReceiptConfirmationDate() {
+        return receiptConfirmationDate;
+    }
+
+    public void setReceiptConfirmationDate(Date receiptConfirmationDate) {
+        this.receiptConfirmationDate = receiptConfirmationDate;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public Date getReviewDate() {
+        return reviewDate;
+    }
+
+    public void setReviewDate(Date reviewDate) {
+        this.reviewDate = reviewDate;
+    }
 }
--- FoodShare/src/advertisingColumn/data/User.java
+++ FoodShare/src/advertisingColumn/data/User.java
@@ -11,4 +11,75 @@ public class User {
     private Date suspensionStart;
     private Date suspensionEnd;
 
+    public void addRating(int newRating) {
+        ratingSum += newRating;
+        ratingCount++;
+        if (newRating <= 2)
+            negativeRatingCount++;
+    }
+
+    public User() {}
+    public User(String name) {
+        username = name;
+        ratingSum = 0;
+        ratingCount = 0;
+        negativeRatingCount = 0;
+        isSuspended = false;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getRatingSum() {
+        return ratingSum;
+    }
+
+    public void setRatingSum(int ratingSum) {
+        this.ratingSum = ratingSum;
+    }
+
+    public int getRatingCount() {
+        return ratingCount;
+    }
+
+    public void setRatingCount(int ratingCount) {
+        this.ratingCount = ratingCount;
+    }
+
+    public int getNegativeRatingCount() {
+        return negativeRatingCount;
+    }
+
+    public void setNegativeRatingCount(int negativeRatingCount) {
+        this.negativeRatingCount = negativeRatingCount;
+    }
+
+    public boolean isSuspended() {
+        return isSuspended;
+    }
+
+    public void setSuspended(boolean suspended) {
+        isSuspended = suspended;
+    }
+
+    public Date getSuspensionStart() {
+        return suspensionStart;
+    }
+
+    public void setSuspensionStart(Date suspensionStart) {
+        this.suspensionStart = suspensionStart;
+    }
+
+    public Date getSuspensionEnd() {
+        return suspensionEnd;
+    }
+
+    public void setSuspensionEnd(Date suspensionEnd) {
+        this.suspensionEnd = suspensionEnd;
+    }
 }
