d770c2db99ff797640a1cc1fa21a6d743799f997
gorecka <gosiula0509@wp.pl>
2022-01-29 12:06:15 +0100

    Dodanie REFUSE przy rezygnacji z nieistniejacej oferty

--- FoodShare/src/advertisingColumn/ProcessResignation.java
+++ FoodShare/src/advertisingColumn/ProcessResignation.java
@@ -28,46 +28,54 @@ public class ProcessResignation extends OneShotBehaviour {
         int offerId = Integer.parseInt(json.getString(JsonKeys.OFFER_ID));
         Offer offer = advertisingColumn.getOfferById(offerId);
 
-        //sprawdzenie, czy rezygnujacy zostal wybrany do oferty
-        boolean isChose = offer.getChosenReceiver().getUsername().equals(receiver.getLocalName());
+        ACLMessage confirmation;
+        JSONObject conf = new JSONObject();
+        conf.put(JsonKeys.OFFER_ID, offerId);
+        //sprawdzenie czy oferta istnieje
+        if (offer == null) {
+            // REFUSE - nie znaleziono oferty
+            conf.put(JsonKeys.MESSAGE, "Offer with the given id was not found");
+            confirmation = new ACLMessage(ACLMessage.REFUSE);
+        } else {
 
-        //znaleznienie wystawiajacego
-        AID giver = new AID(offer.getAuthor().getUsername(), AID.ISLOCALNAME);
+            //sprawdzenie, czy rezygnujacy zostal wybrany do oferty
+            boolean isChose = offer.getChosenReceiver().getUsername().equals(receiver.getLocalName());
+
+            //znaleznienie wystawiajacego
+            AID giver = new AID(offer.getAuthor().getUsername(), AID.ISLOCALNAME);
 
 
+            if (isChose) {
+                ACLMessage notifyGiver;
+                String notification;
+                JSONObject obj = new JSONObject();
 
-        if(isChose) {
-            ACLMessage notifyGiver;
-            String notification;
-            JSONObject obj = new JSONObject();
+                obj.put(JsonKeys.OFFER_RECEIVER_NAME, receiver.getName());
+                obj.put(JsonKeys.OFFER_ID, json.getInt(JsonKeys.OFFER_ID));
+                notification = obj.toString();
+                notifyGiver = new ACLMessage(ACLMessage.INFORM);
+                notifyGiver.setContent(notification);
+                notifyGiver.addReceiver(giver);
+                notifyGiver.setOntology(OntologyNames.RESIGNATION_ONTOLOGY);
+                advertisingColumn.send(notifyGiver);
 
-            obj.put(JsonKeys.OFFER_RECEIVER_NAME, receiver.getName());
-            obj.put(JsonKeys.OFFER_ID, json.getInt(JsonKeys.OFFER_ID));
-            notification = obj.toString();
-            notifyGiver = new ACLMessage(ACLMessage.INFORM);
-            notifyGiver.setContent(notification);
-            notifyGiver.addReceiver(giver);
-            notifyGiver.setOntology(OntologyNames.RESIGNATION_ONTOLOGY);
-            advertisingColumn.send(notifyGiver);
+            }
+            //usun uzytkownika z listy chetnych
+            offer.getPossibleReceivers().removeIf(user -> user.getUsername().equals(receiver.getLocalName()));
+            advertisingColumn.updateOffer(offer);
+
+            //wyslanie potwierdzenia usuniecia z listy chetnych
+            conf.put(JsonKeys.MESSAGE, "Deleted from possible receivers list");
+            conf.put(JsonKeys.OFFER_ID, json.getInt(JsonKeys.OFFER_ID));
+
+            confirmation = new ACLMessage(ACLMessage.AGREE);
+            confirmation.setOntology(OntologyNames.RESIGNATION_ONTOLOGY);
 
         }
-        //usun uzytkownika z listy chetnych
-        offer.getPossibleReceivers().removeIf(user -> user.getUsername().equals(receiver.getLocalName()));
-        advertisingColumn.updateOffer(offer);
-
-        //wyslanie potwierdzenia usuniecia z listy chetnych
-        ACLMessage confirmation;
         String content;
-        JSONObject conf = new JSONObject();
-        conf.put(JsonKeys.MESSAGE, "Deleted from possible receivers list");
-        conf.put(JsonKeys.OFFER_ID, json.getInt(JsonKeys.OFFER_ID));
         content = conf.toString();
-
-        confirmation = new ACLMessage(ACLMessage.AGREE);
-        confirmation.setOntology(OntologyNames.RESIGNATION_ONTOLOGY);
         confirmation.setContent(content);
         confirmation.addReceiver(message.getSender());
-
         advertisingColumn.send(confirmation);
     }
 }
